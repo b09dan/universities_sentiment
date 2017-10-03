a5cbcf85109319f6daaf753d7143edc2535834db
b0gdan <5gcrfncg9asbobyevq8gkvvjt>
2017-10-03 22:33:43 +0300

    Added some comments

--- parsers/timeshighereducation.py
+++ parsers/timeshighereducation.py
@@ -13,10 +13,10 @@ site = 'www.timeshighereducation.com/academic/news/all?page=1'
 site_page = Path(cache_folder + site)
 
 
-def parsing_function(url):
+def parsing_function(url, cache):
     site_tree = urlopen(url).read().decode('utf-8', 'ignore')  # Making url, reading web-page and converting it to utf-8
     site_bs = BeautifulSoup(site_tree, "html.parser")  # Converting to BS type
-    site_titles = site_bs.find_all('a', class_="article-title")  # finding all pics with news link
+    site_titles = site_bs.find_all('a', class_="article-title")
     # articles_links = []
     i = 0
     for site_title in site_titles:
@@ -24,34 +24,34 @@ def parsing_function(url):
         article_title = site_title.get_text()
         print(article_title)
 
-#
+
+# Открывайм файл индекса страниц в кэше
 with open('../uni_cache/index.json', 'r') as articles_index_file:
     articles_index_data = articles_index_file.read()
     articles_index_json = json.loads(articles_index_data)
+    # Пробуем взять название файла страницы, которую хотим парсить, если она не в индексе, выпадет исключение и мы
+    # заменим url обратно на site, чтобы скачать страницу из инета
     try:
         url = '../uni_cache/' + articles_index_json[site]
     except KeyError:
         url = site
-    print(url)
+    # Закрываем файл с индексами
     articles_index_file.close()
+    # Теперь проверям (по содержанию строки uni_cache), какой у нас URL: из кэша или обычный, "интернетовский"
     if url.find('uni_cache') != -1:
-        print('yes')
-        print(url.find('uni_cache'))
+        print('Using web page from cache...')
+        parsing_function(url, cache = True)
     else:
+        # Если страничка не из кэша, то открываем файл с индексами
         articles_index_file = open('../uni_cache/index.json', 'w')
-        site_tree = urlopen('http://'+site).read().decode('utf-8', 'ignore')
-        parsed_page_name = strftime("%d.%m.%Y-%H", gmtime())+'-'+hashlib.md5(site_tree.encode()).hexdigest()
-        articles_index_json[url]=parsed_page_name
-        parsed_page = open('../uni_cache/'+parsed_page_name, 'w')
-        parsed_page.write(site_tree)
+        # Качаем исходники страницы для парсинга
+        site_tree = urlopen('http://' + site).read().decode('utf-8', 'ignore')
+        # Даём ей название по временной метке и хэшу md5
+        parsed_page_name = strftime("%d.%m.%Y-%H", gmtime()) + '-' + hashlib.md5(
+            site_tree.encode()).hexdigest() + '.html'
+        # Кэшируем страницу в папку с кэшами
+        parsed_page = open('../uni_cache/' + parsed_page_name, 'w')
+        parsed_page.write(str(site_tree.encode('utf-8')))
+        # Добавляем страницу в индекс, т.к. теперь она закэширована
+        articles_index_json[url] = parsed_page_name
         json.dump(articles_index_json, articles_index_file, sort_keys=True, indent=4)
-        #parsing_function(url)
-        # articles_index_file.write(str(articles_index_json))
-
-        # if site_page.is_file():
-        #     parsing_function(site_page)
-        # else:
-        #     parsing_function('http://'+site)
-        #     site_page_file = open(site_page)
-        #     site_page_file.write(urlopen('http://'+site).read().decode('utf-8', 'ignore'))
-        #     site_page_file.close()
