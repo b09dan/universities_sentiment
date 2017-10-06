8b7b4eb418f21647048ef43fee2f15d75d82fe55
b0gdan <5gcrfncg9asbobyevq8gkvvjt>
2017-10-07 00:27:47 +0300

    Добавлены напарсенныекэши статей

--- parsers/timeshighereducation.py
+++ parsers/timeshighereducation.py
@@ -8,7 +8,6 @@ import collections
 project_folder = '/home/bogdan/PycharmProjects/universities_sentiment/'
 cache_folder = project_folder + 'cache/'
 site = 'www.timeshighereducation.com'
-site_news_section = '/academic/news/all?page=1'
 
 
 
@@ -34,31 +33,37 @@ def parsing_function(site_tree):
     site_titles = site_bs.find_all('a', class_="article-title")
     articles_data = []
     for site_title in site_titles:
+        # Создаём что-то вроде ассоциативного массива
         article_meta_data = collections.OrderedDict()
-        #article_url = site_title.get('href')
-        #article_title = site_title.get_text()
+        # Херачим в него ссылки статьи
         article_meta_data['article_url'] = site_title.get('href')
+        # Херачим в него заголовки статьи
         article_meta_data['article_title'] = site_title.get_text()
+        # Вкорячиваем этот "ассоциативный массив" в просто массив
         articles_data.append(article_meta_data)
     return articles_data
 
 
-site_tree = cache_function(site + site_news_section)
-articles = parsing_function(site_tree)
-# Здесь будем парсить все статьи,собранные с главной страницы
-for article in articles:
-    site_tree = cache_function(site + article['article_url'])
-    page_article_bs = BeautifulSoup(site_tree, "html.parser")
-    page_article = page_article_bs.find_all('div', class_='field-item even')
-    try:
-        with connection.cursor() as cursor:
-            # Create a new record
-            sql = "INSERT INTO `article` (`article_title`, `article_text`, `article_url`, `article_categories`) VALUES (%s, %s, %s, 'null');"
-            cursor.execute(sql, (article['article_title'], page_article[1].get_text(), article['article_url']))
 
-        # connection is not autocommit by default. So you must commit to save
-        # your changes.
-        connection.commit()
-    finally:
-        print('finally')
+
+
+for i in range(1,500):
+    site_news_section = '/academic/news/all?page='+str(i)
+    site_tree = cache_function(site + site_news_section)
+    articles = parsing_function(site_tree)
+    # Здесь будем парсить все статьи,собранные с главной страницы
+    for article in articles:
+        site_tree = cache_function(site + article['article_url'])
+        page_article_bs = BeautifulSoup(site_tree, "html.parser")
+        page_article = page_article_bs.find_all('div', class_='field-item even')
+        try:
+            with connection.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO `article` (`article_title`, `article_text`, `article_url`, `article_categories`) VALUES (%s, %s, %s, 'null');"
+                cursor.execute(sql, (article['article_title'], page_article[1].get_text(), article['article_url']))
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            connection.commit()
+        finally:
+            print('finally')
 connection.close()
